0fb09d5c379cbff00b260e63e3b759c2dcdf372c
AlphaIcterus <147833854+AlphaIcterus@users.noreply.github.com>
2023-10-17 09:52:44 -0700

    update

--- global/stream/types/types.ts
+++ global/stream/types/types.ts
@@ -1,8 +1,3 @@
 export enum EVENT {
-  service_users = "service-users:event",
-  service_projects = "service-projects:event",
-  service_interactions = "service-interactions:event",
-  service_email = "service-email:event",
-  service_audits = "service-audits:event",
-  service_expiration = "service-expiration:event"
+  micro_store = "micro-store:event",
 }
