3300ee1fc14744b3a992368191b9500a541e1df0
AlphaIcterus <arkastore.co.marketplace@gmail.com>
2023-11-28 11:58:41 -0500

    28-11-2023

--- interface-main/src/pages/store/StorePage.vue
+++ interface-main/src/pages/store/StorePage.vue
@@ -5,12 +5,12 @@
       <div class="store-top-mask" />
 
       <div class="store-top-menu">
-        <div class="store-top-menu-col left">
-          <div class="store-top-menu-button">
+        <div class="column left">
+          <div class="submenu">
             <img src="@/assets/menu.svg" alt="" />
           </div>
 
-          <div class="store-top-menu-nav">
+          <div class="submenu-nav">
             <div
               v-for="item in navTabs"
               :key="item"
@@ -21,8 +21,8 @@
           </div>
         </div>
 
-        <div class="store-top-menu-col center"></div>
-        <div class="store-top-menu-col right"></div>
+        <div class="column center"></div>
+        <div class="column right"></div>
       </div>
 
       <div class="store-top-banner"></div>
@@ -97,8 +97,6 @@ export default {
 </script>
 
 <style lang="css" scoped>
-
-
 .store {
   width: 100%;
   height: 100vh;
@@ -107,7 +105,7 @@ export default {
   background: var(--secondary-b);
 }
 
-.store-top {
+.store .store-top {
   width: inherit;
   height: 600px;
   min-height: 600px;
@@ -120,16 +118,7 @@ export default {
   flex-direction: column;
 }
 
-.store-top-banner {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 2rem;
-  z-index: 100;
-  padding: 0 4rem;
-  margin-top: 2rem;
-}
-
-.store-top-mask {
+.store .store-top .store-top-mask {
   width: inherit;
   height: inherit;
   z-index: 2;
@@ -139,7 +128,7 @@ export default {
   background: var(--secondary-b);
 }
 
-.store-top-menu {
+.store .store-top .store-top-menu {
   padding-left: 4rem;
   margin-top: 74px;
   z-index: 100;
@@ -149,22 +138,47 @@ export default {
   border-top: 1px solid rgb(255, 255, 255, 0.1);
 }
 
-.store-top-menu-nav {
+.store .store-top .store-top-menu .column {
+  flex-basis: 33.33%;
+}
+
+.store .store-top .store-top-menu .column.left {
+  flex-basis: 33.33%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.store .store-top .store-top-menu .column.right {
+  flex-basis: 33.33%;
+  display: flex;
+  justify-content: center;
+}
+
+.store .store-top .store-top-menu .column.center {
+  flex-basis: 66.66%;
+  width: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.store .store-top .store-top-menu .column .submenu-nav {
   display: flex;
   align-items: center;
   padding: 0.25rem;
 }
 
-.store-top-menu-button {
+.store .store-top .store-top-menu .column .submenu {
   cursor: pointer;
   margin-right: 1rem;
 }
 
-.store-top-menu-button img {
+.store .store-top .store-top-menu .column .submenu img {
   width: var(--text-size-d);
 }
 
-.store-top-menu-nav div {
+.store .store-top .store-top-menu .column .submenu-nav div {
   font-size: var(--text-size-a);
   white-space: nowrap;
   cursor: pointer;
@@ -177,33 +191,17 @@ export default {
   border-radius: 999px;
 }
 
-.store-top-menu-nav div:hover {
+.store .store-top .store-top-menu .column .submenu-nav div:hover {
   color: var(--text-w);
   background: rgba(255, 255, 255, 0.1);
 }
 
-.store-top-menu-col {
-  flex-basis: 33.33%;
-}
-
-.store-top-menu-col.left {
-  flex-basis: 33.33%;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
-
-.store-top-menu-col.right {
-  flex-basis: 33.33%;
-  display: flex;
-  justify-content: center;
-}
-
-.store-top-menu-col.center {
-  flex-basis: 66.66%;
-  width: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+.store .store-top .store-top-banner {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2rem;
+  z-index: 100;
+  padding: 0 4rem;
+  margin-top: 2rem;
 }
 </style>
