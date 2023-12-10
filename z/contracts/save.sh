d309ada887a23947505d1a85daf05988ace00543
AlphaIcterus <arkastore.co.marketplace@gmail.com>
2023-12-10 15:06:01 -0500

    10-12-2023

--- z/contracts/save.sh
+++ z/contracts/save.sh
@@ -1,2 +1,2 @@
 cp /home/debian/plutus-apps/plutus-use-cases/src/Plutus/Contracts/Slave.hs .
-cp /home/debian/plutus-apps/plutus-use-cases/test/Spec/Slave.hs .
+cp /home/debian/plutus-apps/plutus-use-cases/test/Spec/Slave.hs ./test
