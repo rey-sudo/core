730286a2fd7d215b22a8e9197dea81d112ba927b
debian <147833854+AlphaIcterus@users.noreply.github.com>
2023-12-15 00:23:18 -0500

    Update SimSlave.hs

--- z/plutus-example/app/SimSlave.hs
+++ z/plutus-example/app/SimSlave.hs
@@ -9,6 +9,8 @@
 {-# LANGUAGE TypeFamilies       #-}
 {-# LANGUAGE TypeOperators      #-}
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE NumericUnderscores #-}
+
 
 module Main
     ( main
@@ -29,11 +31,15 @@ import           Plutus.PAB.Simulator                (SimulatorEffectHandlers)
 import qualified Plutus.PAB.Simulator                as Simulator
 import qualified Plutus.PAB.Webserver.Server         as PAB.Server
 import           Wallet.Emulator.Wallet              (Wallet, knownWallet)
-import           Contracts                       (MarketplaceContracts(..))
+import           Contracts                           (MarketplaceContracts(..))
+import           Deploy                              as D
+import           Slave                as S
+import Plutus.Script.Utils.Ada qualified as Ada
 
 
-defaultWallet :: Wallet
-defaultWallet = knownWallet 1
+sellerWallet' :: Wallet
+sellerWallet' = knownWallet 1
+
 
 main :: IO ()
 main = void $ Simulator.runSimulationWith handlers $ do
@@ -41,9 +47,19 @@ main = void $ Simulator.runSimulationWith handlers $ do
     Simulator.logString @(Builtin MarketplaceContracts) "Starting plutus-starter PAB webserver on port 8080. Press enter to exit."
     shutdown <- PAB.Server.startServerDebug
     Simulator.logString @(Builtin MarketplaceContracts) "********* PAB Server is running *********"
+    void $ liftIO getLine
+    cidInit <- Simulator.activateContract sellerWallet' SlaveContract
+    Simulator.logString @(Builtin MarketplaceContracts) $ "wallet = " ++ show sellerWallet'
+    
+    let sp  = S.StartParams { S.startParams = D.params }
+   
+    void $ Simulator.callEndpointOnInstance cidInit "start" sp
+    Simulator.waitNSlots 2
 
 
 handlers :: SimulatorEffectHandlers (Builtin MarketplaceContracts)
 handlers =
     Simulator.mkSimulatorHandlers def
     $ interpret (contractHandler Builtin.handleBuiltin)
+
+
