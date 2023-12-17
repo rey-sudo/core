f4acb9b64386a51d6e6e6f9b66247c005dec1682
debian <147833854+AlphaIcterus@users.noreply.github.com>
2023-12-17 01:14:28 -0500

    17-12-2023

---
+++ z/plutus-example/app/pab/run-testnet-node.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cardano-node run \
+    --config testnet/testnet-config.json \
+    --topology testnet/testnet-topology.json \
+    --database-path testnet/db \
+    --socket-path testnet/node.sock \
+    --port 3003
--- z/plutus-example/app/pab/run-testnet-pab.sh
+++ z/plutus-example/app/pab/run-testnet-pab.sh
@@ -1,6 +1,8 @@
 #!bin/bash
 
 
-cabal exec plutus-example-deploy -- --config app/pab/testnet/pab-config.yml migrate
-cabal exec plutus-example-deploy -- --config app/pab/testnet/pab-config.yml webserver
+#cabal exec plutus-example-deploy -- --config app/pab/testnet/pab-config.yml migrate
+cabal exec plutus-example-deploy -- --config app/pab/testnet/pab-config.yml webserver --log-config app/pab/loggin.yaml
+
+
 
