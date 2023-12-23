d0f3f5ac4dc144d4dc10481e68bc1d60056d4be9
x <147833854+AlphaIcterus@users.noreply.github.com>
2023-12-23 15:59:51 -0500

    g

--- plutus-example/flake.nix
+++ plutus-example/flake.nix
@@ -1,11 +1,17 @@
 {
-  description = "A very basic flake";
+  description = "marketplace";
+
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    
   };
+
+
 }
