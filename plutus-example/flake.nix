8840b5674263af60e808dbe00dbe11a9e07c4ef4
x <147833854+AlphaIcterus@users.noreply.github.com>
2023-12-23 16:32:49 -0500

    x

--- plutus-example/flake.nix
+++ plutus-example/flake.nix
@@ -1,17 +1,41 @@
 {
   description = "marketplace";
 
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    iogx = {
+      url = "github:input-output-hk/iogx";
+      inputs.hackage.follows = "hackage";
+      inputs.CHaP.follows = "CHaP";
+      inputs.haskell-nix.follows = "haskell-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-  outputs = { self, nixpkgs }: {
+    nixpkgs.follows = "haskell-nix/nixpkgs";
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    hackage = {
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-    
+    CHaP = {
+      url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+      flake = false;
+    };
+
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.hackage.follows = "hackage";
+    };
   };
 
+
+
+  outputs = inputs: inputs.iogx.lib.mkFlake {
+    inherit inputs;
+    repoRoot = ./.;
+    systems = [ "x86_64-linux" "x86_64-darwin" ];
+    outputs = import ./nix/outputs.nix;
+  };
+  
 
 }
