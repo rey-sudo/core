2e8d87be1795432d57e66e10ae5edaf967f5f128
x <147833854+AlphaIcterus@users.noreply.github.com>
2023-12-22 23:38:39 -0500

    Update update.sh

--- service-node/update.sh
+++ service-node/update.sh
@@ -5,34 +5,18 @@ network="preview"
 output="configuration/$network"
 
 
-curl -O -J $source/environments/$network/config.json &&
-jq . config.json | ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' > $output/config.yaml &&
-rm config.json
+curl -s $source/environments/$network/config.json | yq -Poy - > $output/config.yaml 
 
-curl -O -J $source/environments/$network/db-sync-config.json &&
-jq . db-sync-config.json | ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' > $output/db-sync-config.yaml &&
-rm db-sync-config.json
+curl -s $source/environments/$network/db-sync-config.json | yq -Poy -  > $output/db-sync-config.yaml
 
-curl -O -J $source/environments/$network/submit-api-config.json &&
-jq . submit-api-config.json | ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' > $output/submit-api-config.json.yaml &&
-rm submit-api-config.json
+curl -s $source/environments/$network/submit-api-config.json | yq -Poy - > $output/submit-api-config.json.yaml 
 
-curl -O -J $source/environments/$network/topology.json &&
-jq . topology.json | ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' > $output/topology.yaml &&
-rm topology.json
+curl -s $source/environments/$network/topology.json | yq -Poy -  > $output/topology.yaml 
 
-curl -O -J $source/environments/$network/byron-genesis.json &&
-jq . byron-genesis.json | ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' > $output/byron-genesis.yaml &&
-rm byron-genesis.json 
+curl -s $source/environments/$network/byron-genesis.json | yq -Poy -  > $output/byron-genesis.yaml 
 
-curl -O -J $source/environments/$network/shelley-genesis.json &&
-jq . shelley-genesis.json | ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' > $output/shelley-genesis.yaml &&
-rm shelley-genesis.json
+curl -s $source/environments/$network/shelley-genesis.json | yq -Poy -  > $output/shelley-genesis.yaml 
 
-curl -O -J $source/environments/$network/alonzo-genesis.json &&
-jq . alonzo-genesis.json | ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' > $output/alonzo-genesis.yaml &&
-rm alonzo-genesis.json
+curl -s $source/environments/$network/alonzo-genesis.json | yq -Poy -  > $output/alonzo-genesis.yaml 
 
-curl -O -J $source/environments/$network/conway-genesis.json &&
-jq . conway-genesis.json | ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))' > $output/conway-genesis.yaml &&
-rm conway-genesis.json
+curl -s $source/environments/$network/conway-genesis.json | yq -Poy - > $output/conway-genesis.yaml
