25e7dd2c9bec0e1c634af4bca1c961e7e9b4e632
AlphaIcterus <147833854+AlphaIcterus@users.noreply.github.com>
2023-12-28 10:35:33 -0500

    x

--- plutus-example/src/PayToWallet.hs
+++ plutus-example/src/PayToWallet.hs
@@ -35,22 +35,33 @@ import GHC.Generics (Generic)
 
 import Plutus.Contract (ContractError, type (.\/), Endpoint, Contract, Promise, AsContractError(..), endpoint, logInfo, logWarn, ownFirstPaymentPubKeyHash, utxosAt, selectList)
 import Ledger (PaymentPubKeyHash (PaymentPubKeyHash, unPaymentPubKeyHash),)
+import Ledger.Address (CardanoAddress(..), PaymentPrivateKey (PaymentPrivateKey, unPaymentPrivateKey),
+                       PaymentPubKey (PaymentPubKey, unPaymentPubKey),
+                       PaymentPubKeyHash (PaymentPubKeyHash, unPaymentPubKeyHash),
+                       StakePubKey (StakePubKey, unStakePubKey), StakePubKeyHash (StakePubKeyHash, unStakePubKeyHash),
+                       stakePubKeyHashCredential)
+
 import Ledger.Tx.Constraints (mustPayToPubKey)
 import Plutus.Contract (mapError, ownUtxos, ContractError, Endpoint, Promise, adjustUnbalancedTx, endpoint, logInfo, mkTxConstraints,
                         yieldUnbalancedTx)
+import Plutus.Contract.Request (utxosAt)                        
 import Plutus.Script.Utils.Value (Value)
 import Plutus.Script.Utils.Ada qualified as Ada
 import Utilities 
 import Prelude qualified as Haskell
 import PlutusTx.Prelude hiding (Applicative (..), check)
 import Control.Monad (forever, void)
-import Ledger.Tx.Constraints qualified as Constraints
+import Ledger.Tx.Constraints.OffChain qualified as Constraints
 import Utilities (pkhToPubKeyHash)
 import Control.Lens (_2, makeClassyPrisms, review, (^?))
 import PlutusTx qualified
 import PlutusTx.Monoid (inv)
 import Control.Monad.Error.Lens
 import Data.Void (Void, absurd)
+import Cardano.Node.Emulator.Internal.Node.Params qualified as Nparams
+import Ledger.Tx.CardanoAPI.Internal qualified as Tx
+import Plutus.V1.Ledger.Api (Address (Address), Credential (PubKeyCredential), StakingCredential (StakingHash))
+
 
 data StartParams =
     StartParams
@@ -72,14 +83,22 @@ contract = forever endpoints where
 startEndpoint :: Promise () SlaveSchema ContractError ()
 startEndpoint = endpoint @"Start" $ \StartParams{pPriceParam, sWalletParam} -> 
     mapError (review _ContractError) $ do
-    logInfo @Haskell.String "Calling PayToWallet endpoint"
+    logInfo @Haskell.String "1"
+
+    utxos <- utxosAt $ ppkhToAddress (bStoPPKH sWalletParam )
+
+    logInfo @Haskell.String $ "!!!!!!ownUtxos!!!" <> Haskell.show utxos
+    
+    
+
     let price = Ada.toValue $ Ada.lovelaceOf pPriceParam
         ppkh  = bStoPPKH sWalletParam
-        constraints = mustPayToPubKey ppkh price
+        constraints = (mustPayToPubKey ppkh price)
+    
 
     logInfo @Haskell.String "testi"
 
-    utx <- mkTxConstraints @Void Haskell.mempty (mustPayToPubKey ppkh price)
+    utx <- mkTxConstraints @Void Haskell.mempty constraints
     logInfo @Haskell.String $ "Yielding the unbalanced transaction " <> Haskell.show utx
     adjustUnbalancedTx utx >>= yieldUnbalancedTx
 
@@ -89,4 +108,14 @@ startEndpoint = endpoint @"Start" $ \StartParams{pPriceParam, sWalletParam} ->
 bStoPPKH :: Haskell.String -> PaymentPubKeyHash
 bStoPPKH bs = PaymentPubKeyHash $ pkhToPubKeyHash bs
 
+getRight :: Haskell.Either a b -> b
+getRight (Haskell.Right x) = x
+getRight (Haskell.Left _)  = Haskell.error "getRight: Left"
 
+ppkhToAddress :: PaymentPubKeyHash -> CardanoAddress 
+ppkhToAddress = 
+    getRight . Tx.toCardanoAddressInEra Nparams.testnet . plutusAddress 
+    where    
+    plutusAddress ppkh =
+        Address (PubKeyCredential $ unPaymentPubKeyHash ppkh)
+                (Nothing)
