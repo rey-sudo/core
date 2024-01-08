0ede36696d7a51c87299792dfb73f4379fd7cb90
AlphaIcterus <147833854+AlphaIcterus@users.noreply.github.com>
2024-01-08 16:04:39 -0500

    8-1-2024

---
+++ db-schemas.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+alias vtctldclient="vtctldclient --server=localhost:15999"
+alias mysql="mysql -h 127.0.0.1 -P 15306 -u user"
+
+
+
+vtctldclient ApplySchema --sql-file="service-seller/sql/create_seller_scheme.sql" service-seller
+
+vtctldclient ApplyVSchema --vschema-file="service-seller/sql/create_seller_scheme.json" service-seller
--- service-seller/sql/create_seller_scheme.sql
+++ service-seller/sql/create_seller_scheme.sql
@@ -1,15 +1,20 @@
 create table if not exists seller(
-  seller_id bigint not null auto_increment,
+  seller_id varchar(20) not null,
   nickname varchar(50) not null,
   email varbinary(128) not null,
   password_hash varbinary(128) not null,
   verified boolean default false,
+  country varchar(50) not null,
+  completed_sales int default 0,
+  uncompleted_sales int default 0,
+  terms varchar(1000) not null,
   avatar_url varchar(255) not null,
   public_ip varchar(50) not null,
   created_at timestamp default current_timestamp,
-  last_login timestamp default current_timestamp
+  last_login timestamp default current_timestamp,
   schema_t timestamp default current_timestamp,
-  schema_v bigint not null auto_increment,
-  primary key(seller_id, schema_v)
+  schema_v int not null auto_increment,
+  primary key(seller_id),
+  unique key(schema_v)
 ) ENGINE=InnoDB;
 
