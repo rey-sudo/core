2f4fd916f6d152471bb41644602d4f8c6e9b4b6d
AlphaIcterus <147833854+AlphaIcterus@users.noreply.github.com>
2024-01-09 18:44:11 -0500

    9-1-2024

--- secrets.sh
+++ secrets.sh
@@ -1,4 +1,4 @@
-kubectl create secret generic seller-jwt-secret --from-literal=JWT_KEY=kaka-koko-kuku-keke-kiki
+kubectl create secret generic seller-token-secret --from-literal=SELLER_TOKEN_KEY=kaka-koko-kuku-keke-kiki
 
 kubectl create secret generic admin-jwt-secret --from-literal=ADMIN_JWT_KEY=ugly-clear-loyal-free-clean-dirty-fun-happy
 
