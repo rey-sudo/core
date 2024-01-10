b9393e4a63f2d0d128854ac90664d09604cccb7d
AlphaIcterus <147833854+AlphaIcterus@users.noreply.github.com>
2024-01-10 10:42:12 -0500

    10-1-2024

---
+++ kafka.sh
@@ -0,0 +1,5 @@
+kubectl create -f 'https://strimzi.io/install/latest?namespace=default' -n default
+
+kubectl apply -f https://strimzi.io/examples/latest/kafka/kafka-persistent.yaml -n default 
+
+
--- minikube.sh
+++ minikube.sh
@@ -1,1 +1,2 @@
 kubectl config use-context minikube
+minikube start --insecure-registry "10.0.0.0/24"
