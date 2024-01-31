dc088988ebfab34a71a0d70f73a4630e0f632d66
Kraphene <93360398+Kraphene@users.noreply.github.com>
2024-01-31 16:17:18 -0500

    31-1-2024

--- blockchain/plutus-example/pab/testnet/progress.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-export CARDANO_NODE_SOCKET_PATH=$(pwd)/node.sock
-
-echo $CARDANO_NODE_SOCKET_PATH
-
-cardano-cli get-tip --testnet-magic 2
-
--- blockchain/plutus-example/pab/testnet/run-testnet-node.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cardano-node run \
-    --config testnet/config.json \
-    --topology testnet/topology.json \
-    --database-path testnet/db \
-    --socket-path testnet/node.sock \
-    --port 9082
