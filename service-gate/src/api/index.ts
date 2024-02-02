bda6883c30a566b998406712c8068e35c2796d07
Kraphene <93360398+Kraphene@users.noreply.github.com>
2024-02-02 02:14:44 -0500

    2-1-2024

--- service-gate/src/api/index.ts
+++ service-gate/src/api/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 //axios.defaults.withCredentials = true;
 
 const API = axios.create({
-  baseURL: "http://192.168.49.1:9080",
+  baseURL: "http://192.168.65.9:9080",
   timeout: 59000,
   headers: {
     "Content-Type": "application/json"
