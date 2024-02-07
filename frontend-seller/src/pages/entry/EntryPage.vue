cb8bda92dbce458c5b42c2cfb9f0be569916a906
Kraphene <93360398+Kraphene@users.noreply.github.com>
2024-02-07 03:08:58 -0500

    7-1-2024

--- frontend-seller/src/pages/entry/EntryPage.vue
+++ frontend-seller/src/pages/entry/EntryPage.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>x</div>
+  <div class="entry">
+    <LoginView />
+  </div>
 </template>
 
 <script>
+import LoginView from "@/pages/entry/components/LoginView";
 export default {
-  
+  components: {
+    LoginView,
+  },
 };
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.entry {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--blue-a);
+  height: 100vh;
+}
+
+</style>
---
+++ frontend-seller/src/pages/entry/components/LoginView.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="card">
+    <div class="field">
+      <div class="title">Be a seller</div>
+    </div>
+    <div class="field">
+      <InputGroup>
+        <InputGroupAddon>
+          <i class="pi pi-user" />
+        </InputGroupAddon>
+        <InputText placeholder="Email" />
+      </InputGroup>
+    </div>
+    <div class="field">
+      <InputGroup>
+        <InputGroupAddon>
+          <i class="pi pi-user" />
+        </InputGroupAddon>
+        <InputText placeholder="Username" />
+      </InputGroup>
+    </div>
+    <div class="field">
+      <InputGroup>
+        <InputGroupAddon>
+          <i class="pi pi-lock" />
+        </InputGroupAddon>
+        <InputText placeholder="Password" />
+      </InputGroup>
+    </div>
+    <div class="field">
+      <Button type="submit" label="Register" class="button" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const name = ref();
+    const email = ref();
+    const accept = ref(false);
+
+    return {
+      name,
+      email,
+      accept,
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.card {
+  background: var(--base-a);
+  padding: 2rem;
+  border-radius: 18px;
+  display: block;
+}
+
+.field {
+  margin: 1rem 0;
+  width: 300px;
+}
+
+.button {
+  width: 100%;
+}
+
+.title {
+  font-weight: 700;
+  font-size: var(--text-size-g);
+}
+</style>
