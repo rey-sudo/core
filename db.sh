a3d1688a3b8d73ebb0390c711c78e386cb3c50c6
Kraphene <93360398+Kraphene@users.noreply.github.com>
2024-02-18 10:53:26 -0500

    x

--- db.sh
+++ db.sh
@@ -9,11 +9,13 @@ GRANT ALL PRIVILEGES ON *.* TO 'marketplace'@'%';
 
 GRANT CREATE, ALTER, DROP, INDEX, CREATE TEMPORARY TABLES, LOCK TABLES, REFERENCES ON *.* TO 'marketplace'@'%';
 
+GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'marketplace'@'%';
 
 ALTER USER 'marketplace'@'%' IDENTIFIED BY 'password';
 
 
-
+SELECT variable_value as "BINARY LOGGING STATUS (log-bin) ::"
+FROM performance_schema.global_variables WHERE variable_name='log_bin';
 
 
 FLUSH PRIVILEGES;
