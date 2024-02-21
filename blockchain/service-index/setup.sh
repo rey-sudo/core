eb2aadb455df2ba240522feaa9163e9629c924c1
Kraphene <93360398+Kraphene@users.noreply.github.com>
2024-02-21 11:17:43 -0500

    21-2-2024

--- blockchain/service-index/setup.sh
+++ blockchain/service-index/setup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-ldd_output=$(ldd ./bin/plutus-chain-index)
+WORKDIR=$(pwd)
+NETWORK=preview
+network_id=2
+index_bin_path="$WORKDIR/bin/"
+index_binary="$index_bin_path/plutus-chain-index"
+config_path="$WORKDIR/bin/configuration/$NETWORK"
+
+
+if [ ! -x "$index_binary" ]; then
+    echo "Error: binary not found in /bin."
+    exit 1
+else
+    echo "plutus-chain-index binary found."
+fi
+
+ldd_output=$(ldd $index_binary)
 
 paths=$(echo "$ldd_output" | awk -F '/' '/=> \/nix\/store\// {print $4}')
 
@@ -11,3 +26,31 @@ for folder in $paths; do
     sudo cp -r "$source_path" "$destination"
     echo "Copied: $folder"
 done
+
+
+export PATH=$PATH:$index_bin_path
+
+echo "PATH=>: $PATH"
+
+cd ..
+
+ROOTDIR=$(pwd)
+
+node_config_path="$ROOTDIR/service-node/bin/configuration/$NETWORK"
+
+export PATH=$PATH:$node_config_path
+
+echo "NODE_PATH: $PATH"
+
+cd $WORKDIR
+
+
+plutus-chain-index --socket-path $node_config_path/db/node.socket \
+    --db-path $index_bin_path/index.db \
+    --port 9083 \
+    --network-id 2 \
+     start-index
+
+
+
+    
