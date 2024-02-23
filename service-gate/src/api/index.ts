161a638ef8cd4532f09698f3a675c18a57126a2b
Kraphene <93360398+Kraphene@users.noreply.github.com>
2024-02-23 17:38:07 -0500

    23-2-2024

--- service-gate/src/api/index.ts
+++ service-gate/src/api/index.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 //axios.defaults.withCredentials = true;
+//kubectl get nodes -o wide
 
 const API = axios.create({
   baseURL: "http://192.168.65.9:9080",
-  timeout: 59000,
+  timeout: 10000,
   headers: {
     "Content-Type": "application/json"
   },
--- service-gate/src/routes/create-slot.ts
+++ service-gate/src/routes/create-slot.ts
@@ -24,6 +24,7 @@ const createSlotHandler = async (req: Request, res: Response) => {
   };
 
   let connection: any = null;
+  
   try {
     connection = await DB.client.getConnection();
 
