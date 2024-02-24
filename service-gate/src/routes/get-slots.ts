b308c4b91ad192e32a52857afab8b09d9797a87f
Kraphene <93360398+Kraphene@users.noreply.github.com>
2024-02-24 12:08:51 -0500

    Update get-slots.ts

--- service-gate/src/routes/get-slots.ts
+++ service-gate/src/routes/get-slots.ts
@@ -17,10 +17,23 @@ const getSlotsHandler = async (req: Request, res: Response) => {
     connection = await DB.client.getConnection();
 
     const [response] = await connection.execute(
-      `SELECT *
-       FROM product
-       JOIN slot ON product.product_id = slot.product_id
-       WHERE product.seller_id = ?;
+      `
+      SELECT 
+        p.*,
+        JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'slot_id', s.slot_id,
+                'status', s.status
+            )
+        ) AS slot_array
+      FROM 
+        product p
+      LEFT JOIN 
+        slot s ON p.product_id = s.product_id
+      WHERE
+        p.seller_id = ?
+      GROUP BY 
+        p.product_id;      
       `,
       [SELLER.seller_id]
     );
