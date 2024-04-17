bf2c1dd2db15b99245097e58e36a5051b0843755
Kraphene <sudo.delete.evil@gmail.com>
2024-04-16 21:52:14 -0500

    Update GridWrap.vue

--- frontend-home/src/pages/home/components/GridWrap.vue
+++ frontend-home/src/pages/home/components/GridWrap.vue
@@ -129,8 +129,8 @@ export default {
   gap: 20px;
   padding: 0 1rem;
   min-height: 100vh;
-  border-top-left-radius: 16px;
-  border-top-right-radius: 16px;
+  border-top-left-radius: 28px;
+  border-top-right-radius: 28px;
   background: var(--base-a);
 }
 
