4fb95295af51b08e96763c5f6261b022b548e22f
Kraphene <sudo.delete.evil@gmail.com>
2024-04-18 17:26:01 -0500

    Update ProductPage.vue

--- frontend-home/src/pages/product/ProductPage.vue
+++ frontend-home/src/pages/product/ProductPage.vue
@@ -10,7 +10,7 @@
             :numVisible="1"
             :circular="true"
             :transitionInterval="0"
-            containerStyle="max-width: 70%; min-height: 500px; max-height: 500px; margin-top: 6rem;"
+            containerStyle="max-width: 70%; min-height: 500px; max-height: 500px; margin-top: 5rem;"
             :showItemNavigators="false"
             :showThumbnails="false"
           >
