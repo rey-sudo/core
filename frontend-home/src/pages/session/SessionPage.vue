fdb4bac2c32b0c53583fec419728981f9b451fa2
Juan Rey <sudo.delete.evil@gmail.com>
2024-05-31 12:16:32 -0500

    Update SessionPage.vue

--- frontend-home/src/pages/session/SessionPage.vue
+++ frontend-home/src/pages/session/SessionPage.vue
@@ -6,7 +6,7 @@
         <span>Slot order: 783819023812030 </span>
       </div>
 
-      <div class="session-header-bottom"> 
+      <div class="session-header-bottom">
         <span>Space for bilateral negotiation</span>
         <span>Time Created: may 27 8008 :80:am</span>
       </div>
@@ -25,7 +25,11 @@
 
       <!---RIGHT-->
       <div class="session-top-right">
-        <div class="session-top-right-chat"></div>
+        <div class="session-top-right-chat">
+          <div>x</div>
+
+          <div>text</div>
+        </div>
       </div>
       <!---RIGHT-->
     </div>
@@ -39,30 +43,29 @@ export default {};
 
 <style lang="css" scoped>
 .session-header {
-  height: 100px;
+  height: 150px;
   background: rgba(255, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 1rem;
-
 }
 
-.session-header-top, .session-header-bottom {
+.session-header-top,
+.session-header-bottom {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
 
-
-.session-header-top span:nth-child(1){
-    font-size: var(--text-size-e);
-    font-weight: 600;
+.session-header-top span:nth-child(1) {
+  font-size: var(--text-size-e);
+  font-weight: 600;
 }
 
-.session-header-top span:nth-child(2){
-    font-size: var(--text-size-e);
-    font-weight: 600;
+.session-header-top span:nth-child(2) {
+  font-size: var(--text-size-e);
+  font-weight: 600;
 }
 
 .session {
@@ -77,7 +80,6 @@ export default {};
   display: flex;
   align-items: flex-start;
   padding: 1rem;
-  
 }
 
 .session-top-left {
@@ -95,6 +97,21 @@ export default {};
   border-radius: 24px;
   margin-left: auto;
   border: 1px solid var(--border-a);
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+}
+
+.session-top-right-chat div:nth-child(1) {
+  height: 100px;
+  border-bottom: 1px solid var(--border-a);
+}
+
+.session-top-right-chat div:nth-child(2) {
+  background: var(--base-a);
+  min-height: 700px;
+  overflow-y: scroll;
+  overflow-x: hidden;
 }
 
 .session-bottom {
