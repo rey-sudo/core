0f6f875ecde592c37cecfc2a5f7c7413f7a8fc2c
Juan Rey <sudo.delete.evil@gmail.com>
2024-06-09 08:29:02 -0500

    9-6-2024

--- z/secrets/secrets.sh
+++ z/secrets/secrets.sh
@@ -1,5 +1,7 @@
 kubectl create secret generic seller-token-secret --from-literal=SELLER_TOKEN_KEY=kaka-koko-kuku-keke-kiki
 
+kubectl create secret generic user-token-secret --from-literal=SELLER_TOKEN_KEY=hoho-hehe-hihi-hohu-hehe
+
 kubectl create secret generic admin-jwt-secret --from-literal=ADMIN_JWT_KEY=ugly-clear-loyal-free-clean-dirty-fun-happy
 
 kubectl create secret generic encrypto-secret --from-literal=ENCRYPTO_KEY=password567567567
