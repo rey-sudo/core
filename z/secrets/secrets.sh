3818ecad48a1b709adb3d1ec18983b325345c312
Juan Rey <sudo.delete.evil@gmail.com>
2024-06-09 08:31:11 -0500

    Update secrets.sh

--- z/secrets/secrets.sh
+++ z/secrets/secrets.sh
@@ -1,6 +1,6 @@
 kubectl create secret generic seller-token-secret --from-literal=SELLER_TOKEN_KEY=kaka-koko-kuku-keke-kiki
 
-kubectl create secret generic user-token-secret --from-literal=SELLER_TOKEN_KEY=hoho-hehe-hihi-hohu-hehe
+kubectl create secret generic user-token-secret --from-literal=USER_TOKEN_KEY=hoho-hehe-hihi-hohu-hehe
 
 kubectl create secret generic admin-jwt-secret --from-literal=ADMIN_JWT_KEY=ugly-clear-loyal-free-clean-dirty-fun-happy
 
