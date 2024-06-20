6ccc22aefca932c4a712511c7db6597ffb681b27
Juan Rey <sudo.delete.evil@gmail.com>
2024-06-19 21:36:36 -0500

    Update MiniChat.vue

--- frontend-home/src/pages/session/components/MiniChat.vue
+++ frontend-home/src/pages/session/components/MiniChat.vue
@@ -2,7 +2,7 @@
   <div class="chat">
     <div class="chat-top">
       <img
-        src="https://api.dicebear.com/9.x/lorelei-neutral/svg?seed=cheapking800"
+        src="https://api.dicebear.com/9.x/initials/svg?seed=cheapking700"
         alt=""
       />
 
