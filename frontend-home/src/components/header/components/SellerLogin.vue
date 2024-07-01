1ec05eb8a9152b56dff57e62c4b3fed35c94b1c3
Juan Rey <sudo.delete.evil@gmail.com>
2024-07-01 17:48:02 -0500

    Update SellerLogin.vue

--- frontend-home/src/components/header/components/SellerLogin.vue
+++ frontend-home/src/components/header/components/SellerLogin.vue
@@ -1,17 +1,54 @@
 <template>
-    <div>
-x
+  <div class="card flex justify-content-center p-fluid">
+    <div v-focustrap class="w-full" style="max-width: 20rem">
+      <div class="field">
+        <IconField>
+          <InputIcon>
+            <i class="pi pi-user" />
+          </InputIcon>
+          <InputText
+            id="input"
+            v-model="name"
+            type="text"
+            placeholder="Name"
+            autofocus
+          />
+        </IconField>
+      </div>
+      <div class="field">
+        <IconField>
+          <InputIcon>
+            <i class="pi pi-envelope" />
+          </InputIcon>
+          <InputText
+            id="email"
+            v-model="email"
+            type="email"
+            placeholder="Email"
+          />
+        </IconField>
+      </div>
+      <div class="field-checkbox">
+        <Checkbox id="accept" v-model="accept" name="accept" value="Accept" />
+        <label for="accept">I agree to the terms and conditions.</label>
+      </div>
+      <Button type="submit" label="Submit" class="mt-2" />
     </div>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
 
-export default{
+export default {
+  setup() {
+    const name = ref();
+    const email = ref();
+    const accept = ref(false);
 
-}
-
+    return { name, email, accept };
+  },
+};
 </script>
 
-<style lang="css" scoped>
-
-</style>
+<style lang="css" scoped></style>
