6b300632ae83793c9e270b4d289466f97fe21f24
Juan Rey <sudo.delete.evil@gmail.com>
2024-07-03 19:44:41 -0500

    Update SlotsView.vue

--- frontend-seller/src/pages/dashboard/components/SlotsView.vue
+++ frontend-seller/src/pages/dashboard/components/SlotsView.vue
@@ -331,9 +331,8 @@
       </template>
     </Dialog>
     <Toast />
-    <!--DIALOG SECTION-->
-
-    <!---CONTENT-->
+    <!---//////////////////////////////////////////////////////CONTENT////////////////////////////-->
+    
     <div class="slots-wrap">
       <div class="slots-card">
         <DataTable
@@ -748,183 +747,10 @@ export default {
       minProductImages: 5,
       slotListDialogVisible: false,
       slotListDialogIndex: null,
-      customers: [
-        {
-          id: 1000,
-          name: "James Butt",
-          country: {
-            name: "Algeria",
-            code: "dz",
-          },
-          company: "Benton, John B Jr",
-          date: "2015-09-13",
-          status: "unqualified",
-          verified: true,
-          activity: 17,
-          representative: {
-            name: "Ioni Bowcher",
-            image: "ionibowcher.png",
-          },
-          balance: 70663,
-        },
-        {
-          id: 1001,
-          name: "Josephine Darakjy",
-          country: {
-            name: "Egypt",
-            code: "eg",
-          },
-          company: "Chanay, Jeffrey A Esq",
-          date: "2019-02-09",
-          status: "proposal",
-          verified: true,
-          activity: 0,
-          representative: {
-            name: "Amy Elsner",
-            image: "amyelsner.png",
-          },
-          balance: 82429,
-        },
-        {
-          id: 1002,
-          name: "Art Venere",
-          country: {
-            name: "Panama",
-            code: "pa",
-          },
-          company: "Chemel, James L Cpa",
-          date: "2017-05-13",
-          status: "qualified",
-          verified: false,
-          activity: 63,
-          representative: {
-            name: "Asiya Javayant",
-            image: "asiyajavayant.png",
-          },
-          balance: 28334,
-        },
-        {
-          id: 1003,
-          name: "Lenna Paprocki",
-          country: {
-            name: "Slovenia",
-            code: "si",
-          },
-          company: "Feltz Printing Service",
-          date: "2020-09-15",
-          status: "new",
-          verified: false,
-          activity: 37,
-          representative: {
-            name: "Xuxue Feng",
-            image: "xuxuefeng.png",
-          },
-          balance: 88521,
-        },
-        {
-          id: 1004,
-          name: "Donette Foller",
-          country: {
-            name: "South Africa",
-            code: "za",
-          },
-          company: "Printing Dimensions",
-          date: "2016-05-20",
-          status: "proposal",
-          verified: true,
-          activity: 33,
-          representative: {
-            name: "Asiya Javayant",
-            image: "asiyajavayant.png",
-          },
-          balance: 93905,
-        },
-        {
-          id: 1005,
-          name: "Simona Morasca",
-          country: {
-            name: "Egypt",
-            code: "eg",
-          },
-          company: "Chapman, Ross E Esq",
-          date: "2018-02-16",
-          status: "qualified",
-          verified: false,
-          activity: 68,
-          representative: {
-            name: "Ivan Magalhaes",
-            image: "ivanmagalhaes.png",
-          },
-          balance: 50041,
-        },
-      ],
+
       maxProductImages: 5,
       selectedProducts: null,
       filters: {},
-      categories: [
-        {
-          name: "Home",
-          code: "home",
-        },
-        {
-          name: "Electronics",
-          code: "electronics",
-        },
-        {
-          name: "Fashion",
-          code: "fashion",
-        },
-        {
-          name: "Beauty",
-          code: "beauty",
-        },
-        {
-          name: "Toys",
-          code: "toys",
-        },
-        {
-          name: "Tools",
-          code: "tools",
-        },
-        {
-          name: "Sports",
-          code: "sports",
-        },
-        {
-          name: "Health",
-          code: "health",
-        },
-        {
-          name: "Books",
-          code: "books",
-        },
-        {
-          name: "Automotive",
-          code: "automotive",
-        },
-        {
-          name: "Appliances",
-          code: "appliances",
-        },
-        {
-          name: "Furniture",
-          code: "furniture",
-        },
-      ],
-      statuses: [
-        {
-          label: "STOCK",
-          value: "stock",
-        },
-        {
-          label: "LOW",
-          value: "low",
-        },
-        {
-          label: "OUT",
-          value: "out",
-        },
-      ],
     };
   },
   created() {
