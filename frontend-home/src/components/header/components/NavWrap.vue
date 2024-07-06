64e8cad338cfedf8b5879c9e3b97297e60268b87
Juan Rey <sudo.delete.evil@gmail.com>
2024-07-06 16:39:04 -0500

    6-7-2024

--- frontend-home/src/components/header/components/NavWrap.vue
+++ frontend-home/src/components/header/components/NavWrap.vue
@@ -114,7 +114,7 @@ export default {
   left: 0;
   width: 100%;
   align-items: center;
-  background: var(--black-a);
+  background: var(--primary-c);
   color: var(--text-w);
   font-weight: 500;
   border-top: 1px solid transparent;
