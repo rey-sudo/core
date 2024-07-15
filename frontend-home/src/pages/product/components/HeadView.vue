e3caff7ba7a763a083d2bda9f2dd0e472ce6169a
Juan Rey <sudo.delete.evil@gmail.com>
2024-07-15 16:07:50 -0500

    Update HeadView.vue

--- frontend-home/src/pages/product/components/HeadView.vue
+++ frontend-home/src/pages/product/components/HeadView.vue
@@ -532,7 +532,7 @@ export default {
 .head .head-rating span {
   margin-right: 0.5rem;
   font-size: var(--text-size-c);
-  color: var(--text-a);
+  color: var(--primary-c);
   font-weight: 600;
 }
 
