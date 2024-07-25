1a29a1cc945d3f7a5550564a23773bfcff2ab40b
Juan Rey <sudo.delete.evil@gmail.com>
2024-07-25 13:26:37 -0500

    25-7-2024

--- frontend-home/src/pages/session/components/SellerChat.vue
+++ frontend-home/src/pages/session/components/SellerChat.vue
@@ -9,8 +9,12 @@
       <ul id="messages"></ul>
     </div>
     <div class="chat-bottom">
-      <div id="editorElement"></div>
-      <button @click="sendMessage" id="send">Send</button>
+      <div class="chat-wrap">
+        <div id="editorElement"></div>
+        <button class="chat-wrap-send" @click="sendMessage">
+          <i class="pi pi-send" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -18,6 +22,8 @@
 <script>
 import headerAPI from "@/components/header/composable/header-api";
 import StarterKit from "@tiptap/starter-kit";
+import Placeholder from "@tiptap/extension-placeholder";
+import TextAlign from "@tiptap/extension-text-align";
 import { Editor } from "@tiptap/core";
 import { io } from "socket.io-client";
 import { HOST } from "@/api/index";
@@ -70,8 +76,26 @@ export default {
   mounted() {
     this.editor = new Editor({
       element: document.getElementById("editorElement"),
-      extensions: [StarterKit],
-      content: "<p>Hello World!</p>",
+      editorProps: {
+        attributes: {
+          class: "editorClass",
+        },
+      },
+      extensions: [
+        StarterKit,
+        Text,
+        TextAlign.configure({
+          alignments: ["left"],
+          defaultAlignment: "left",
+        }),
+        Placeholder.configure({
+          placeholder: "write a message…",
+          emptyEditorClass: "is-editor-empty",
+          emptyNodeClass: "my-custom-is-empty-class",
+        }),
+      ],
+      editable: true,
+      content: "",
     });
 
     const messages = document.getElementById("messages");
@@ -83,10 +107,72 @@ export default {
       window.scrollTo(0, document.body.scrollHeight);
     });
   },
+  beforeUnmount() {
+    this.editor.destroy();
+  },
 };
 </script>
 
 <style lang="css" scoped>
+::v-deep(.tiptap p.is-editor-empty:first-child::before) {
+  color: var(--text-b);
+  content: attr(data-placeholder);
+  float: left;
+  opacity: 0.5;
+  font-size: var(--text-size-b);
+  height: 0;
+  font-weight: 500;
+  pointer-events: none;
+}
+
+::v-deep(.tiptap p.is-empty::before) {
+  color: var(--text-b);
+  content: attr(data-placeholder);
+  float: left;
+  height: 0;
+  pointer-events: none;
+}
+::v-deep(.editorClass) {
+  width: 330px;
+  padding: 0.5rem;
+  border: 1px solid var(--border-b);
+  border-radius: 6px;
+  font-weight: 600;
+  text-align: left;
+  font-size: var(--text-size-b);
+  max-height: 100px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+::v-deep(.editorClass::-webkit-scrollbar) {
+  display: none;
+}
+
+.chat-wrap-send {
+  border: none;
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--base-a);
+  cursor: pointer;
+}
+
+.chat-wrap-send i {
+  transform: rotate(45deg);
+  color: var(--text-b);
+  font-size: var(--text-size-e) !important;
+}
+
+.chat-wrap {
+  display: flex;
+  align-items: center;
+}
+
 .chat {
   background: var(--base-a);
   width: 400px;
@@ -107,7 +193,8 @@ export default {
 }
 
 .chat-bottom {
-  height: 100px;
+  padding: 1rem;
+  border-top: 1px solid var(--border-a);
 }
 
 .chat-top {
