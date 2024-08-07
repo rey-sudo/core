ea70e27625769c350b3a4a681021af426d7c9c55
Juan Rey <sudo.delete.evil@gmail.com>
2024-08-07 16:39:46 -0500

    7-8-2024

--- marketplace/deno/index.js
+++ marketplace/deno/index.js
@@ -1,13 +1,10 @@
 import { ColdWallet, Core, Blaze, Blockfrost } from "@blaze-cardano/sdk";
 
-
-
-let address = Core.addressFromBech32(
+let externalWallet = Core.addressFromBech32(
   "addr_test1qppygdhzm0t7nnlclmds3dy0wc3du870dpy48juu0xxuu2aefdfvc4e0785y7vfhwlmsn3rn26mzvv9md0mhnkpjlc4s0jshh4"
 );
 
-
-const externalWallet = Core.addressFromBech32(
+const targetWallet = Core.addressFromBech32(
   "addr_test1qp539v654clv34y7k6zwrtxzczwvzz0dudmgfy5rt3qvjf2hrg74pzy4umh8udkhshpqmwdzluk6zvr5tcrj8h74re2q2yavu8"
 );
 
@@ -18,17 +15,21 @@ const provider = new Blockfrost({
 
 
 
-const wallet = new ColdWallet(address, 2, provider);
+const wallet = new ColdWallet(externalWallet, 2, provider);
 
 console.log("Your blaze address: ", wallet.address.toBech32());
 
 const blaze = await Blaze.from(provider, wallet);
 
-
 const tx = await blaze
   .newTransaction()
-  .payLovelace(externalWallet, 5n * 1_000_000n)
+  .payLovelace(targetWallet, 10n * 1_000_000n)
+  .addRequiredSigner("424436e2dbd7e9cff8fedb08b48f7622de1fcf684953cb9c798dce2b")
   .complete();
 
 // Dump the transaction for you to submit securely
-console.log(`Please sign and submit this transaction: ${tx.toCbor()}`);
+console.log(tx.toCbor());
+
+
+
+//"previewgTjbjYtdKdOcNmhtu6H9snNl3DhnaxQf"
