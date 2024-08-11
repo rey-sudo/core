2ffec5c9a26d7beb47f886ea139eefd33d30984e
Juan Rey <sudo.delete.evil@gmail.com>
2024-08-11 13:26:09 -0500

    11-8-2024

--- service-gateway/src/index.ts
+++ service-gateway/src/index.ts
@@ -1,2 +1,15 @@
+import { Blockfrost, Lucid } from "lucid-cardano";
+import {
+    ColdWallet,
+    Core,
+    Data,
+    Blaze,
+    makeValue,
+  } from "@blaze-cardano/sdk";
+  import { applyParamsToScript, cborToScript } from "@blaze-cardano/uplc";
+
+console.log(Blockfrost, Lucid);
+console.log(Data);
+
 const message: string = "Hello, TypeScript with Node.js!";
 console.log(message);
--- z/secrets/secrets.sh
+++ z/secrets/secrets.sh
@@ -2,7 +2,7 @@ kubectl create secret generic seller-token-secret --from-literal=SELLER_TOKEN_KE
 
 kubectl create secret generic user-token-secret --from-literal=USER_TOKEN_KEY=hoho-hehe-hihi-hohu-hehe
 
-kubectl create secret generic admin-jwt-secret --from-literal=ADMIN_JWT_KEY=ugly-clear-loyal-free-clean-dirty-fun-happy
+kubectl create secret generic admin-token-secret --from-literal=ADMIN_JWT_KEY=ugly-clear-loyal-free-clean-dirty-fun-happy
 
 kubectl create secret generic encrypto-secret --from-literal=ENCRYPTO_KEY=password567567567
 
